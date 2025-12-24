56cf550bb862c2823551df0bb3a215fc601aa295
abhayybhatt <abhayy.bhatt.w@gmail.com>
2025-12-25 00:44:46 +0530

    Swith to SPA mode for Vercel deployment

--- api/server.js
+++
@@ -1,8 +0,0 @@
-import pkg from "@react-router/node";
-const { createRequestHandler } = pkg;
-import * as build from "../build/server/index.js";
-
-export default createRequestHandler({
-  build,
-  mode: process.env.NODE_ENV || "production",
-});
